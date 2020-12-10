284b5bf0997d214e19b7834729079ad67b781e67
samuelevi87 <samuel.levi.alves@outlook.com>
2020-12-10 20:52:51 -0300

    Implementando o switch nos botões

--- app/src/main/java/com/sl3v1/myfirstapp/MainActivity.java
+++ app/src/main/java/com/sl3v1/myfirstapp/MainActivity.java
@@ -8,40 +8,54 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     EditText num1, num2;
-    Button botao;
-    TextView mostraSoma, mostraSubtracao, mostraMultiplicacao, mostraDivisao;
+    Button btSoma, btSub, btMult, btDiv;
+    TextView mostraResultado;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         num1 = findViewById(R.id.edt_num1);
         num2 = findViewById(R.id.edt_num2);
-        botao = findViewById(R.id.somar);
-        mostraSoma = findViewById(R.id.mostrarSoma);
-        mostraSubtracao = findViewById(R.id.mostrarSubtracao);
-        mostraMultiplicacao = findViewById(R.id.mostrarMultiplicacao);
-        mostraDivisao = findViewById(R.id.mostrarDivisao);
+
+        btSoma = findViewById(R.id.btSoma);
+        btSub = findViewById(R.id.btSub);
+        btMult = findViewById(R.id.btMult);
+        btDiv = findViewById(R.id.btDiv);
+
+        mostraResultado = findViewById(R.id.mostrarResultado);
+
+        btSoma.setOnClickListener((this));
+        btSub.setOnClickListener((this));
+        btMult.setOnClickListener((this));
+        btDiv.setOnClickListener((this));
     }
 
-    public void soma(View view) {
-        Double soma = (Double.parseDouble((num1.getText().toString()))) + (Double.parseDouble((num2.getText().toString())));
-        mostraSoma.setText((String.valueOf(String.format("%.2f", soma))));
-    }
-    public void subtracao(View view) {
-        Double subtracao = (Double.parseDouble((num1.getText().toString()))) - (Double.parseDouble((num2.getText().toString())));
-        mostraSubtracao.setText((String.valueOf(String.format("%.2f", subtracao))));
-    }
-    public void multiplicacao(View view) {
-        Double multiplicacao = (Double.parseDouble((num1.getText().toString()))) * (Double.parseDouble((num2.getText().toString())));
-        mostraMultiplicacao.setText((String.valueOf(String.format("%.2f", multiplicacao))));
-    }
-    public void divisao(View view) {
-        Double divisao = (Double.parseDouble((num1.getText().toString()))) / (Double.parseDouble((num2.getText().toString())));
-        mostraDivisao.setText((String.valueOf(String.format("%.2f", divisao))));
+    @Override
+    public void onClick(View v) {
+//        Double mNum1 = Double.parseDouble(num1.getText().toString());
+//        Double mNum2 = Double.parseDouble(num2.getText().toString());
+6
+        switch (v.getId()) {
+            case R.id.btSoma:
+                Double soma = (Double.parseDouble((num1.getText().toString()))) + (Double.parseDouble((num2.getText().toString())));
+                mostraResultado.setText((String.valueOf(String.format("%.2f", soma))));
+                break;
+            case R.id.btSub:
+                Double subtrair = (Double.parseDouble((num1.getText().toString()))) - (Double.parseDouble((num2.getText().toString())));
+                mostraResultado.setText((String.valueOf(String.format("%.2f", subtrair))));
+            case R.id.btMult:
+                Double multiplicacao = (Double.parseDouble((num1.getText().toString()))) * (Double.parseDouble((num2.getText().toString())));
+                mostraResultado.setText((String.valueOf(String.format("%.2f", multiplicacao))));
+                break;
+            case R.id.btDiv:
+                Double divisao = (Double.parseDouble((num1.getText().toString()))) / (Double.parseDouble((num2.getText().toString())));
+                mostraResultado.setText((String.valueOf(String.format("%.2f", divisao))));
+        }
     }
 }
